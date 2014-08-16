95081321bce29e9804cb76f2e32f329f355efce2
Isamu Mogi <saturday6c@gmail.com>
2014-08-16 13:37:27 +0900

    To be able to change shells for test

--- test/init.sh
+++ test/init.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 PATH="`dirname $0`/../bin:$PATH"
+if [ -n "${ZSH_VERSION:-}" ]; then
+  setopt shwordsplit
+  SHUNIT_PARENT=$0
+fi
--- test/run.sh
+++ test/run.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 cd `dirname $0`
+
+if [ "$HSENV_TEST_SHELL" = "" ]; then
+  HSENV_TEST_SHELL=/bin/sh
+fi
+
 for sh in *_test.sh; do
   echo "[$sh]"
-  /bin/sh $sh
+  $HSENV_TEST_SHELL $sh
   if [ $? -ne 0 ]; then
     exit 1
   fi
