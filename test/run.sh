37dd0682190cb430743837c64dceec0809351dc6
Isamu Mogi <saturday6c@gmail.com>
2014-08-16 14:16:30 +0900

    Test all target shells

--- test/run.sh
+++ test/run.sh
@@ -2,14 +2,35 @@
 
 cd `dirname $0`
 
+run_test() {
+  echo HSENV_TEST_SHELL=$HSENV_TEST_SHELL
+  for t in *_test.sh; do
+    echo "[$t]"
+    $HSENV_TEST_SHELL $t
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
+  done
+}
+
+if [ "$1" = "all" ]; then
+  for sh in ash bash dash ksh mksh posh zsh; do
+    which $sh > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+      echo $sh not found
+      continue
+    fi
+    HSENV_TEST_SHELL=$sh
+    run_test
+    if [ $? -ne 0 ]; then
+      exit 1
+    fi
+  done
+  exit
+fi
+
 if [ "$HSENV_TEST_SHELL" = "" ]; then
   HSENV_TEST_SHELL=/bin/sh
 fi
 
-for sh in *_test.sh; do
-  echo "[$sh]"
-  $HSENV_TEST_SHELL $sh
-  if [ $? -ne 0 ]; then
-    exit 1
-  fi
-done
+run_test
