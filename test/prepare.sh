04df8a1f341f1ea56aad51c8b933e07557233f74
Isamu Mogi <saturday6c@gmail.com>
2014-08-16 22:31:19 +0900

    Add osx configuration

---
+++ test/prepare.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd `dirname $0`
+case `../libexec/config.guess | awk -F- '{print $3}'` in
+  linux)
+    sudo apt-get update -qq
+    sudo apt-get install -qq ash bash dash ksh mksh posh zsh
+    ;;
+  darwin*)
+    brew update
+    brew install dash ksh mksh
+    ;;
+esac
