d2d72832c85076872eef3dcefbae68e2780b1d55
Isamu Mogi <saturday6c@gmail.com>
2014-08-16 21:51:01 +0900

    Colored output

--- test/run.sh
+++ test/run.sh
@@ -3,14 +3,43 @@
 cd `dirname $0`
 
 run_test() {
+  tmpdir=../tmp/run_test
+  rm -fr $tmpdir
+  mkdir -p $tmpdir
   echo HSENV_TEST_SHELL=$HSENV_TEST_SHELL
   for t in *_test.sh; do
     echo "[$t]"
-    $HSENV_TEST_SHELL $t
-    if [ $? -ne 0 ]; then
+    error_regexp="shunit2:ERROR"
+    esc=`echo -e "\033"`
+    rm -f $tmpdir/succeed
+    if [ "$HSENV_TEST_COLOR" = "yes" ]; then
+      ($HSENV_TEST_SHELL $t 2>&1 && touch $tmpdir/succeed) \
+        | sed -e "s/^\(${error_regexp}\)/$esc[31;1m\1$esc[m/" \
+        | sed -e "s/^\(ASSERT:\)/$esc[31;1m\1$esc[m/" \
+        | sed -e "s/^\(FAILED .*\)/$esc[31;1m\1$esc[m/" \
+        | tee $tmpdir/test_result.txt
+    else
+      ($HSENV_TEST_SHELL $t 2>&1 && touch $tmpdir/succeed) \
+        | tee $tmpdir/test_result.txt
+    fi
+    if [ ! -e $tmpdir/succeed ]; then
+      exit 1
+    fi
+    if grep -E "$error_regexp" $tmpdir/test_result.txt > /dev/null; then
+      if [ "$HSENV_TEST_COLOR" = "yes" ]; then
+        echo -e "\n$esc[31;1mFAILED$esc[m"
+      else
+        echo "FAILED"
+      fi
       exit 1
     fi
   done
+
+  if [ "$HSENV_TEST_COLOR" = "yes" ]; then
+    echo -e "\n$esc[32;1mSUCCEED$esc[m"
+  else
+    echo "SUCCEED"
+  fi
 }
 
 if [ "$1" = "all" ]; then
@@ -31,6 +60,14 @@ fi
 
 if [ "$HSENV_TEST_SHELL" = "" ]; then
   HSENV_TEST_SHELL=/bin/sh
+fi
+
+if [ "$HSENV_TEST_COLOR" = "" ]; then
+  if [ -t ]; then
+    HSENV_TEST_COLOR=yes
+  else
+    HSENV_TEST_COLOR=no
+  fi
 fi
 
 run_test
--- test/watch.sh
+++ test/watch.sh
@@ -5,15 +5,12 @@ tmpdir=../tmp/watch
 rm -fr $tmpdir
 mkdir -p $tmpdir
 while true; do
-  ./run.sh > $tmpdir/next.txt 2>&1
+  HSENV_TEST_COLOR=yes ./run.sh > $tmpdir/next.txt 2>&1
   testresult=$?
   if ! cmp $tmpdir/previous.txt $tmpdir/next.txt; then
     echo -ne "\033c" # clear screen
     cat $tmpdir/next.txt
     cp $tmpdir/next.txt $tmpdir/previous.txt
-    if [ $testresult -eq 0 ]; then
-        echo -e "\nSUCCEED"
-    fi
   fi
   sleep 1
 done
