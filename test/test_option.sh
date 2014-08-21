aa5ae3a127eaa2a6513d22cfc01516ec75d112b9
Isamu Mogi <saturday6c@gmail.com>
2014-08-21 19:56:23 +0900

    Add url check

--- test/test_option.sh
+++ test/test_option.sh
@@ -17,4 +17,11 @@ test_is_version() {
   assertFalse "is_version 1.1 2"
 }
 
+test_is_url() {
+  assertTrue  "is_url http://a"
+  assertTrue  "is_url https://bb"
+  assertFalse "is_url ttps://bb"
+  assertFalse "is_url ttp://bb"
+}
+
 . shunit2/src/shunit2
