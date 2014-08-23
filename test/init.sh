faa1673c3a9aa4c26e1d05d06561eaca8a794037
Isamu Mogi <saturday6c@gmail.com>
2014-08-23 14:00:09 +0900

    `make' command selection

--- test/init.sh
+++ test/init.sh
@@ -13,3 +13,11 @@ _hsenv_test_datadir="$_hsenv_private_root_dir/data"
 if [ -z "$HSENV_TEST_LEVEL" ]; then
   HSENV_TEST_LEVEL=0
 fi
+
+if [ -z "$HSENV_TEST_MAKE" ]; then
+  if [ which gmake > /dev/null 2> /dev/null ]; then
+    HSENV_TEST_MAKE=gmake
+  else
+    HSENV_TEST_MAKE=make
+  fi
+fi
