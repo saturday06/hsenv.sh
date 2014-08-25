00f9b8de9fc0743fad6e95ff372733997bfa5e43
Isamu Mogi <saturday6c@gmail.com>
2014-08-25 21:16:47 +0900

    Always create tmp dir for test

--- test/init.sh
+++ test/init.sh
@@ -11,6 +11,8 @@ fi
 _hsenv_test_tmp_dir="$_hsenv_private_root_dir/tmp/test"
 _hsenv_test_data_dir="$_hsenv_private_root_dir/test/data"
 
+mkdir -p $_hsenv_test_tmp_dir
+
 if [ -z "$HSENV_TEST_LEVEL" ]; then
   HSENV_TEST_LEVEL=0
 fi
