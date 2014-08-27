ab78bfadebf5af2b08084696e4777d73c73ccbd7
Isamu Mogi <saturday6c@gmail.com>
2014-08-27 20:58:55 +0900

    Shorten installation function name

--- test/test_option.sh
+++ test/test_option.sh
@@ -78,11 +78,11 @@ test_installation_of() {
   assertEquals "transplant_local_executable" "`installation_of ''`"
   assertEquals "install_remote_source" "`installation_of 7.8.3`"
   assertEquals "install_remote_source" "`installation_of 1.2`"
-  assertEquals "install_remote_binary" "`installation_of http://aa`"
-  assertEquals "install_remote_binary" "`installation_of https://x/y.tar.xz`"
-  assertEquals "install_remote_binary" "`installation_of http://a.b/c.tar.bz2`"
-  assertEquals "install_local_binary" "`installation_of foo`"
-  assertEquals "install_local_binary" "`installation_of foo-1.0.tar.xz`"
+  assertEquals "install_remote" "`installation_of http://aa`"
+  assertEquals "install_remote" "`installation_of https://x/y.tar.xz`"
+  assertEquals "install_remote" "`installation_of http://a.b/c.tar.bz2`"
+  assertEquals "install_local" "`installation_of foo`"
+  assertEquals "install_local" "`installation_of foo-1.0.tar.xz`"
 }
 
 . shunit2/src/shunit2
