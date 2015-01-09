8e154e19f33da3566faaf5b56cb438b486b6ae7a
Isamu Mogi <isamu@leafytree.jp>
2015-01-09 21:01:37 +0900

    Binary distribution detector for Solaris x86

--- libexec/binchecker/src/solaris/ix86/app.s
+++ libexec/binchecker/src/solaris/ix86/app.s
@@ -2,5 +2,6 @@
 .global _start
 _start:
     push $0
+    push $0
     movl $1, %eax
     int $0x91
--- libexec/binchecker/src/solaris/v11.sh
+++ libexec/binchecker/src/solaris/v11.sh
@@ -3,43 +3,35 @@
 set -e
 export LC_ALL=C
 
-out=ix86-pc-freebsd8
+out=ix86-pc-solaris2.11
 echo === $out ===
 gcc -m32 -s -nostartfiles -o ../../$out \
-  /lib/libdl.so.2 \
-  /lib/libgcc_s.so.1 \
-  /lib/libm.so.6 \
-  /lib/libpthread.so.0 \
+  /lib/libdl.so.1 \
+  /lib/libpthread.so.1 \
   /lib/librt.so.1 \
-  /lib/libtinfo.so.5 \
-  /lib/libutil.so.1 \
-  /usr/lib/libgmp.so.10 \
+  /usr/lib/libffi.so.5 \
+  /usr/lib/libgcc_s.so.1 \
+  /usr/lib/libgmp.so.3 \
+  /usr/lib/libncurses.so.5 \
   ix86/app.s
-objdump -adx ../../$out > $out.info
 ldd ../../$out > $out.out.ldd
 awk '{print $1}' $out.out.ldd | sort > $out.solist
-awk '{print $1}' ix86/gmp5.ghc-7_8_4.ldd | grep -v "^libHS" | grep -v "^libffi\.so\.6$" | sort > gmp5.ghc-7_8_4.solist
-diff -u gmp5.ghc-7_8_4.solist $out.solist
+awk '{print $1}' ix86/v11.ghc-7_8_3.ldd | grep -v "^libHS" | sort > v11.ghc-7_8_3.solist
+diff -u v11.ghc-7_8_3.solist $out.solist
 ../../$out
 
-out=x86_64-unknown-freebsd8
+out=ix86-pc-solaris2.10
 echo === $out ===
-gcc -m64 -s -nostartfiles -o ../../$out \
-  /lib64/libc.so.6 \
-  /lib64/libdl.so.2 \
-  /lib64/libgcc_s.so.1 \
-  /lib64/libm.so.6 \
-  /lib64/libpthread.so.0 \
-  /lib64/librt.so.1 \
-  /lib64/libtinfo.so.5 \
-  /lib64/libutil.so.1 \
-  /usr/lib64/libgmp.so.10 \
-  x86_64/app.s
-objdump -adx ../../$out > $out.info
+gcc -m32 -s -nostartfiles -o ../../$out \
+  /lib/libcurses.so.1 \
+  /lib/libdl.so.1 \
+  /lib/libpthread.so.1 \
+  /lib/librt.so.1 \
+  ix86/app.s
 ldd ../../$out > $out.out.ldd
 awk '{print $1}' $out.out.ldd | sort > $out.solist
-awk '{print $1}' x86_64/gmp5.ghc-7_8_4.ldd | grep -v "^libHS" | grep -v "^libffi\.so\.6$" | sort > gmp5.ghc-7_8_4.solist
-diff -u gmp5.ghc-7_8_4.solist $out.solist
+awk '{print $1}' ix86/v10.ghc-7_8_3.ldd | grep -v "^libHS" | sort > v10.ghc-7_8_3.solist
+diff -u v10.ghc-7_8_3.solist $out.solist
 ../../$out
 
 echo done
