0e0a55434067f445af7b1f8006b727cf9ec38cf3
Isamu Mogi <isamu@leafytree.jp>
2015-01-10 02:40:18 +0900

    Move freebsd's build script to the dir for its arch

--- libexec/binchecker/src/freebsd/x86_64/v8.sh
+++ libexec/binchecker/src/freebsd/x86_64/v8.sh
@@ -15,15 +15,15 @@ gcc -m64 -s -o $out \
   /usr/local/lib/libcharset.so.1 \
   /usr/local/lib/libgmp.so.10 \
   /usr/local/lib/libiconv.so.3 \
-  x86_64/app.s
+  app.s
 sed -i.bak -e 's/libiconv\.so\.2/libiconv.so.3/' $out
 rm $out.bak
 objdump -adx $out > $out.info
 ldd $out > $out.out.ldd
 tail -n +2 $out.out.ldd | awk '{print $1}' | sort > $out.solist
-tail -n +2 x86_64/ghc-7_8_3.ldd  | awk '{print $1}' | grep -v "^libHS" | grep -v "^libffi\.so\.6$" | sort > ghc-7_8_3.solist
+tail -n +2 ghc-7_8_3.ldd  | awk '{print $1}' | grep -v "^libHS" | grep -v "^libffi\.so\.6$" | sort > ghc-7_8_3.solist
 diff -u ghc-7_8_3.solist $out.solist
 ./$out
-mv $out ../../
+mv $out ../../../
 
 echo done
